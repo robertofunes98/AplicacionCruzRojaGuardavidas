83adea0f694fef187e820a476569e9cd089cc3cc
robertofunes98 <robertofunes98@gmail.com>
2019-01-09 01:10:43 -0600

    - Limpiezza de importaciones inutilizadas en las clases

--- app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/RegistroUsuarios.java
+++ app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/RegistroUsuarios.java
@@ -3,12 +3,10 @@ package sv.company.give.cruzrojaguardavidas.fragmentos;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
--- app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/ReunionesAdministrador.java
+++ app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/ReunionesAdministrador.java
@@ -2,8 +2,6 @@ package sv.company.give.cruzrojaguardavidas.fragmentos;
 
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -14,24 +12,8 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import sv.company.give.cruzrojaguardavidas.ConexionWebService;
-import sv.company.give.cruzrojaguardavidas.DatePickerFragment;
 import sv.company.give.cruzrojaguardavidas.R;
 import sv.company.give.cruzrojaguardavidas.fragmentos.child_fragments.AgregarReunion;
 import sv.company.give.cruzrojaguardavidas.fragmentos.child_fragments.NotificarReunion;
@@ -72,20 +54,6 @@ public class ReunionesAdministrador extends Fragment {
         TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        /*Creando viewpager que contendra la info que cargaran las tabs*/
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        //mSectionsPagerAdapter = new SectionsPagerAdapter(myContext.getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        //mViewPager = (ViewPager) rootView.findViewById(R.id.container);
-       // mViewPager.setAdapter(mSectionsPagerAdapter);
-
-       // TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
-
-        //mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        //tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
         return  rootView;
     }
@@ -96,71 +64,7 @@ public class ReunionesAdministrador extends Fragment {
         super.onAttach(activity);
     }
 
-    /*Funciones para la carga de sub fragmentos*/
-    /*
-
-
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            args.putString("cookie",cookie);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_agregar_reunion, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getArguments().getString("cookie"));//textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 4;
-        }
-    }*/
-
-    //Probando xd
+    //Funciones para la carga de fragmentos en las tabs
 
     static class Adapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragments = new ArrayList<>();
