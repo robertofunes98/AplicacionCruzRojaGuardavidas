552dfa14087ad6d584c207acfc800a7fcb3276ba
robertofunes98 <robertofunes98@gmail.com>
2019-03-05 13:18:36 -0600

    - Funcion para guardar por patron agregada

--- app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/child_fragments/AgregarEntreno.java
+++ app/src/main/java/sv/company/give/cruzrojaguardavidas/fragmentos/child_fragments/AgregarEntreno.java
@@ -4,15 +4,16 @@ package sv.company.give.cruzrojaguardavidas.fragmentos.child_fragments;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -34,15 +35,18 @@ import sv.company.give.cruzrojaguardavidas.Variables;
  */
 public class AgregarEntreno extends Fragment {
     //Variable que guardara la cookie en el fragment al recibirla
-    String cookie="";
+    String cookie="", diasPatron="";
     ConexionWebService conexion;
     JSONObject jsonObjeto=null;
 
     Spinner spPatron;
-    RelativeLayout rlPatron, rlDiaUnico;
+    ConstraintLayout rlPatron, rlDiaUnico;
 
-    Button btnGuardar;
-    EditText etHora,etFecha,etLugar;
+    Button btnGuardarDiaUnico, btnGuardarPatron;
+    EditText etHora,etFecha,etLugar, etHoraPatron, etLugarPatron;
+
+    //CheckBoxs de dias
+    CheckBox cbLunes, cbMartes, cbMiercoles, cbJueves, cbViernes, cbSabado, cbDomingo, arrayCheckBoxDias[];
 
     public AgregarEntreno() {
         // Required empty public constructor
@@ -60,14 +64,31 @@ public class AgregarEntreno extends Fragment {
         cookie = getArguments().getString("cookie");
 
         spPatron=rootView.findViewById(R.id.spPatron);
-        rlDiaUnico=rootView.findViewById(R.id.rlContenidoDiaUnico);
-        rlPatron=rootView.findViewById(R.id.rlContenidoPatron);
+        rlDiaUnico=rootView.findViewById(R.id.clContenidoDiaUnico);
+        rlPatron=rootView.findViewById(R.id.clContenidoPatron);
 
-        btnGuardar=rootView.findViewById(R.id.btnGuardar);
+        btnGuardarDiaUnico=rootView.findViewById(R.id.btnGuardarDiaUnico);
+        btnGuardarPatron=rootView.findViewById(R.id.btnGuardarPatron);
+
         etFecha=rootView.findViewById(R.id.etFecha);
         etHora=rootView.findViewById(R.id.etHora);
         etLugar=rootView.findViewById(R.id.etLugar);
+        etHoraPatron=rootView.findViewById(R.id.etHoraPatron);
+        etLugarPatron=rootView.findViewById(R.id.etLugarPatron);
 
+
+        //Inicialiacion de checkboxs
+        cbLunes=rootView.findViewById(R.id.cbLunes);
+        cbMartes=rootView.findViewById(R.id.cbMartes);
+        cbMiercoles=rootView.findViewById(R.id.cbMiercoles);
+        cbJueves=rootView.findViewById(R.id.cbJueves);
+        cbViernes=rootView.findViewById(R.id.cbViernes);
+        cbSabado =rootView.findViewById(R.id.cbSabado);
+        cbDomingo=rootView.findViewById(R.id.cbDomingo);
+        //El array se inicializa hasta aqui para que contenga objetos no nulos
+        arrayCheckBoxDias=new CheckBox[]{cbLunes, cbMartes, cbMiercoles, cbJueves, cbViernes, cbSabado, cbDomingo};
+
+        //EditText modificados para obtener fecha
         etFecha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,6 +104,7 @@ public class AgregarEntreno extends Fragment {
             }
         });
 
+        //editText modificados para obtener hora
         etHora.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,7 +124,29 @@ public class AgregarEntreno extends Fragment {
             }
         });
 
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
+        etHoraPatron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hora,minutos;
+                final Calendar c = Calendar.getInstance();
+                hora=c.get(Calendar.HOUR_OF_DAY);
+                minutos=c.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if(minute==0)
+                            etHoraPatron.setText(hourOfDay+":"+minute+"0");
+                        else
+                            etHoraPatron.setText(hourOfDay+":"+minute);
+                    }
+                },hora,minutos,false);
+
+                timePickerDialog.show();
+            }
+        });
+
+        //botones
+        btnGuardarDiaUnico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -114,7 +158,7 @@ public class AgregarEntreno extends Fragment {
                     try {
                         //conexion.execute(url,parametros,cookie)
                         String resultado=conexion.execute(Variables.url+"entrenos.php",
-                                "accion=guardarEntreno&fecha="+etFecha.getText()+"&hora="+etHora.getText()+":00"+"&lugar="+etLugar.getText(),cookie).get();
+                                "accion=guardarEntrenoDiaUnico&fecha="+etFecha.getText()+"&hora="+etHora.getText()+":00"+"&lugar="+etLugar.getText(),cookie).get();
 
                         //Toast.makeText(getContext(),cookie,Toast.LENGTH_LONG).show();
 
@@ -130,6 +174,48 @@ public class AgregarEntreno extends Fragment {
                             etLugar.setText("");
                             etHora.setText("");
                             etFecha.setText("");
+                        }
+
+                    } catch (ExecutionException | InterruptedException | JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        btnGuardarPatron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diasPatron="";
+                for (int i=0; i<arrayCheckBoxDias.length; i++) {
+                    if(arrayCheckBoxDias[i].isChecked())
+                        diasPatron+=String.valueOf(i);
+                }
+
+                if(etHoraPatron.getText().toString().isEmpty() || etLugarPatron.getText().toString().isEmpty())
+                    Toast.makeText(getContext(),"Por favor complete todos los campos",Toast.LENGTH_SHORT).show();
+                else
+                {
+                    conexion=new ConexionWebService();
+                    try {
+                        //conexion.execute(url,parametros,cookie)
+                        String resultado=conexion.execute(Variables.url+"entrenos.php",
+                                "accion=guardarEntrenoPatron&hora="+etHoraPatron.getText()+":00"+"&lugar="+etLugarPatron.getText()
+                                        +"&diasPatron="+diasPatron,cookie).get();
+
+                        //Toast.makeText(getContext(),cookie,Toast.LENGTH_LONG).show();
+
+                        JSONArray jsonRespuesta= new JSONArray(resultado);
+
+                        jsonObjeto=jsonRespuesta.getJSONObject(0);
+
+                        if(jsonObjeto.has("error"))
+                            Toast.makeText(getContext(),jsonObjeto.getString("error"),Toast.LENGTH_LONG).show();
+                        else {
+                            Toast.makeText(getContext(), jsonObjeto.getString("resultado"), Toast.LENGTH_LONG).show();
+                            etLugarPatron.setText("");
+                            etHoraPatron.setText("");
                         }
 
                     } catch (ExecutionException | InterruptedException | JSONException e) {
@@ -161,6 +247,7 @@ public class AgregarEntreno extends Fragment {
 
             }
         });
+
 
         return rootView;
     }
